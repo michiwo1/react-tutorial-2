256ab7c4b1bbc95c8d3e66d0db1307882e3981d8
michiwo <54099086+michiwo1@users.noreply.github.com>
2021-04-14 15:59:16 +0900

    前回の復習

---
+++ src/ChildArea.jsx
@@ -0,0 +1,18 @@
+export const ChildArea = (props) => {
+  const { open } = props;
+  const style = {
+    width: "100%",
+    height: "200px",
+    backgroundColor: "khaki"
+  };
+
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+        </div>
+      ) : null}
+    </>
+  );
+};
