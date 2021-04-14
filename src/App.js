d07724ef28bd1b33f45f2ca1701ebdc4fd49eadd
michiwo <54099086+michiwo1@users.noreply.github.com>
2021-04-14 16:20:50 +0900

    memo, useCallbackの使い方

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useReducer, useState } from "react";
+import { useCallback, useReducer, useState } from "react";
 import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
@@ -14,13 +14,15 @@ export default function App() {
     setOpen(!open);
   };
 
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,18 +1,27 @@
-export const ChildArea = (props) => {
-  const { open } = props;
+import { memo } from "react";
+
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+  console.log("ChildAreaが再レンダリングされた！！");
   const style = {
     width: "100%",
     height: "200px",
     backgroundColor: "khaki"
   };
 
+  const data = [...Array(100).keys()];
+  data.forEach(() => {
+    console.log("...");
+  });
+
   return (
     <>
       {open ? (
         <div style={style}>
           <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
         </div>
       ) : null}
     </>
   );
-};
+});
